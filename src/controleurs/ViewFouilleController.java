bf4c674d1d3940c89090d5499df218ebec6d7447
Tagre <mael.clouet@hotmail.fr>
2018-02-28 16:51:57 +0100

    jeu fouille grossièrement fonctionnel sans gestion score

--- src/controleurs/ViewFouilleController.java
+++ src/controleurs/ViewFouilleController.java
@@ -2,6 +2,7 @@ package controleurs;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javax.xml.bind.JAXBContext;
@@ -10,24 +11,33 @@ import javax.xml.bind.Unmarshaller;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import modeles.Fouille;
 import modeles.Instruction;
 
 public class ViewFouilleController implements Initializable{
 	
 	private String xml="";
-	private boolean soloBloc=true;
-	private int cmptChronologie=0;
-	private String xmlChronologie="";
-	private boolean son;
-	private int score;
+	private static boolean soloBloc=true;
+	private static int cmptChronologie=0;
+	private static String xmlChronologie="";
+	private static boolean son;
+	private static int score;
 	
 	private Fouille fouille;
+	private static ArrayList<String> intitules= new ArrayList<String>();
+	private static int cmpt=0;
 	
 	@FXML
 	AnchorPane AP_Game,AP_Inventory;
@@ -58,7 +68,11 @@ public class ViewFouilleController implements Initializable{
 		imageFond.setImage(image);
 		centerImage(imageFond);
 		
+		
+		
 		for(Instruction instruction : fouille.getListeInstructions()){
+			intitules.add(instruction.getIntitule());
+			
 			file = new File(instruction.getImageObjet());
 			image = new Image(file.toURI().toString());
 			ImageView element=new ImageView(image);
@@ -69,12 +83,17 @@ public class ViewFouilleController implements Initializable{
 	        {
 	            @Override
 	            public void handle(MouseEvent t) {
-	            	AP_Game.getChildren().remove(element);
-	            	AP_Inventory.getChildren().add(element);
+	            	if (LB_Instruction.getText().equals(instruction.getIntitule())){
+	            		AP_Game.getChildren().remove(element);
+	            		AP_Inventory.getChildren().add(element);
+	            		majInstruction(LB_Instruction);
+	            	}
+	            	
 	            }
 	        });
 		}
 		
+		majInstruction(LB_Instruction);
 	}
 	
 	public void setXML(String xml) {
@@ -124,6 +143,31 @@ public class ViewFouilleController implements Initializable{
         }
     }
 
-	
+	public static void majInstruction(Label label){
+		if (cmpt<intitules.size()){
+			label.setText(intitules.get(cmpt));
+			cmpt++;
+		} else {
+			Alert alert = new Alert(AlertType.INFORMATION, "Le jeu fouille est fini.\n Votre Score : "+ score, ButtonType.OK);
+			alert.showAndWait();
+			
+			if(soloBloc){
+				Stage stage = (Stage) label.getScene().getWindow();
+				stage.close();
+			} else {
+				Stage stage = (Stage) label.getScene().getWindow();
+				Node node=JFxUtils.loadNextBloc(cmptChronologie, xmlChronologie, son);
+				if (node!=null){
+					stage.setScene(new Scene((Parent) node, 850, 650));
+				} else {
+					stage.close();
+				}
+			}
+			
+			
+		}
+		
+		
+	}
 
 }
